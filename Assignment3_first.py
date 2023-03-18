26117cb1d8d4a5cbb567191cc772db514760c3f2
SurajMadankar <127080716+SurajMadankar@users.noreply.github.com>
2023-03-18 23:19:08 +0530

    Add files via upload

---
+++ Assignment3_first.py
@@ -0,0 +1,15 @@
+
+#sample_list=(8, 2, 3, 0, 7)
+sample_list=(8, 2, 3, 0, 7)
+sum=0
+for i in sample_list:
+    sum+=i
+    print(sum)
+
+#or
+#sample_list=[8, 2, 3, 0, 7]
+sample_list=[8, 2, 3, 0, 7]
+sum=0
+for i in sample_list:
+    sum+=i
+    print(sum)
